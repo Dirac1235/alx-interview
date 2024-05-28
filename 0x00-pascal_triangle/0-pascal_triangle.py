ca1925ea0b136771267f269e42a6ba7a97e26455
Webi Muleta <webimuleta01@gmail.com>
2024-05-28 17:42:54 +0300

    edit

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -3,7 +3,7 @@ def pascal_triangle(n):
     superlis = [[1]]
     for i in range(n-1):
         prefix = lis.copy()
-        
+
         for j in range(1, len(lis)):
             prefix[j] = lis[j-1] + lis[j]
         lis = prefix.copy()
@@ -11,16 +11,3 @@ def pascal_triangle(n):
             lis.append(1)
             superlis.append(lis)
     return superlis
-        
-pascal_triangle = __import__('0-pascal_triangle').pascal_triangle
-
-def print_triangle(triangle):
-    """
-    Print the triangle
-    """
-    for row in triangle:
-        print("[{}]".format(",".join([str(x) for x in row])))
-
-
-if __name__ == "__main__":
-    print_triangle(pascal_triangle(5))

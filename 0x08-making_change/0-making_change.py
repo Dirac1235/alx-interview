1a17c51ac4f3761cfcbb2f8bc89701d22b1b138a
Webi Muleta <webimuleta01@gmail.com>
2024-07-28 08:57:23 +0300

    some changes

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -6,10 +6,10 @@ def makeChange(coins, total):
     """returns the minimum coins to make change"""
     coins.sort()
     count = 0
-    for coin in range(len(coins)-1, -1):
-        while total - coin >= 0:
+    for coin in range(len(coins)-1, -1, -1):
+        while total - coins[coin] >= 0:
             count += 1
-            total -= total - coin
-    if coin:
+            total -= coins[coin]
+    if total:
         return -1
     return count

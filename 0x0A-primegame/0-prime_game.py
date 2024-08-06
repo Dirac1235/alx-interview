3e3f1d7792426cc78d22cf6c89c252deace2e762
Webi Muleta <webimuleta01@gmail.com>
2024-08-06 17:58:59 +0300

    correction

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -5,8 +5,8 @@
 def isWinner(x, nums):
     """returns the winner for the prime game"""
     players = [0, 0]
-    turn = 0
     for j in range(x):
+        turn = 0
         rounds = [0, 0]
         primecontainer = [[k, True] for k in range(nums[j] + 1)]
         for i in range(2, len(primecontainer)):
@@ -17,7 +17,7 @@ def isWinner(x, nums):
                 else:
                     rounds[1] += 1
                     turn = 0
-                    add = i
+                add = i
                 while i + add < len(primecontainer):
                     primecontainer[i + add][1] = False
                     i += add

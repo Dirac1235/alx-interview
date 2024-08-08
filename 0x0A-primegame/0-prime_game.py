7e6851e9accdd2430ac943f1dc0b5b60ba07385d
Webi Muleta <webimuleta01@gmail.com>
2024-08-08 21:46:44 +0300

    correction on 0-prime_game.py

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,31 +1,35 @@
 #!/usr/bin/python3
-"""Solution file for Prime Game"""
+""" Prime Game Problem
+    Topic: Eratosthenes algorithm
+"""
 
 
 def isWinner(x, nums):
-    """returns the winner for the prime game"""
-    players = [0, 0]
-    for j in range(x):
-        turn = 0
-        rounds = [0, 0]
-        primecontainer = [[k, True] for k in range(nums[j] + 1)]
-        for i in range(2, len(primecontainer)):
-            if primecontainer[i][1]:
-                if turn == 0:
-                    rounds[0] += 1
-                    turn = 1
-                else:
-                    rounds[1] += 1
-                    turn = 0
-                add = i
-                while i + add < len(primecontainer):
-                    primecontainer[i + add][1] = False
-                    i += add
-        if rounds[0] > rounds[1]:
-            players[0] += 1
+    """
+    Determines the winner in the prime game using
+    Eratosthenes prime sieving algorithm
+    """
+    Ben = 0
+    Maria = 0
+
+    for round in range(x):
+        playing_numbers = [num for num in range(2, nums[round] + 1)]
+        index = 0
+        # Sieve prime numbers per round
+        while (index < len(playing_numbers)):
+            current_prime = playing_numbers[index]
+            sieve_index = index + current_prime
+            while(sieve_index < len(playing_numbers)):
+                playing_numbers.pop(sieve_index)
+                sieve_index += current_prime - 1
+            index += 1
+
+        prime_count = (len(playing_numbers))
+        if prime_count and prime_count % 2:
+            Maria += 1
         else:
-            players[1] += 1
-    if players[0] > players[1]:
-        return "Maria"
-    else:
-        return "Ben"
+            Ben += 1
+
+    if Ben == Maria:
+        return None
+    return 'Ben' if Ben > Maria else 'Maria'
